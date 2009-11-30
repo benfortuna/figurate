72f1a2617e6a6f877fc743e8c578ff6b73e5a1a8
fortuna <fortuna@localhost>
2009-11-30 11:15:01 +0000

    Use substance laf

--- src/main/groovy/org/mnode/figurate/Figurate.groovy
+++ src/main/groovy/org/mnode/figurate/Figurate.groovy
@@ -1,5 +1,6 @@
 /**
- * This file is part of Base Modules.
+
+* This file is part of Base Modules.
  *
  * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
  *
@@ -30,81 +31,49 @@ import javax.swing.JScrollPane
 import javax.swing.UIManager
 import groovy.swing.SwingXBuilder
 import groovy.swing.LookAndFeelHelper
+import org.jvnet.substance.SubstanceLookAndFeel
+import org.jvnet.substance.api.SubstanceConstants
  /**
   * @author fortuna
   *
   */
+  /*
+@Grapes([
+    @Grab(group='org.codehaus.griffon.swingxbuilder', module='swingxbuilder', version='0.1.6'),
+    @Grab(group='net.java.dev.substance', module='substance', version='5.3'),
+    @Grab(group='net.java.dev.substance', module='substance-swingx', version='5.3'),
+    @Grab(group='org.swinglabs', module='swingx', version='0.9.2'),
+    @Grab(group='jgoodies', module='forms', version='1.0.5'),
+    @Grab(group='org.codehaus.griffon.flamingobuilder', module='flamingobuilder', version='0.2'),
+    @Grab(group='net.java.dev.flamingo', module='flamingo', version='4.2'),
+    @Grab(group='org.apache.xmlgraphics', module='batik-awt-util', version='1.7')])
+    */
 public class Figurate {
- 		static def tabs
- 		
- 	static void main(def args) {
-         UIManager.put("TabbedPane.contentBorderInsets", new Insets(0, 0, 0, 0));
-         LookAndFeelHelper.instance.addLookAndFeelAlias('substance5', 'org.jvnet.substance.skin.SubstanceBusinessLookAndFeel')
+         
+     static void main(def args) {
+         UIManager.put("TabbedPane.contentBorderInsets", new Insets(0, 0, 0, 0))
+         UIManager.put(org.jvnet.lafwidget.LafWidget.ANIMATION_KIND, org.jvnet.lafwidget.utils.LafConstants.AnimationKind.FAST.derive(2))
+        LookAndFeelHelper.instance.addLookAndFeelAlias('substance5', 'org.jvnet.substance.skin.SubstanceNebulaLookAndFeel')
          def swing = new SwingXBuilder()
          swing.edt {
-             lookAndFeel('substance5', 'system')
+             lookAndFeel('substance5') //, 'system')
          }
 
          def headingFont = new Font('Arial', Font.PLAIN, 14)
          def textFont = new Font('Courier', Font.PLAIN, 12)
-
- 		def newTab = {
- 			tabs.add(swing.panel(name: 'New File') {
- 				borderLayout()
- 				editorPane(page: new java.net.URL('file:C:\\WINDOWS\\system32\\drivers\\etc\\hosts'), font: textFont)
- 			})
- 		}
- 		
- 		 tabs = swing.tabbedPane(tabLayoutPolicy: JTabbedPane.SCROLL_TAB_LAYOUT) {
-                 panel(name: 'Home') {
+         def newTab = {
+             tabs.add(swing.edt {
+                 panel(name: 'New File') {
                      borderLayout()
-                     vbox(constraints: BorderLayout.CENTER) {
-                         panel(constraints: BorderLayout.CENTER, border: emptyBorder(10)) {
-                             borderLayout()
-                             label('Recent Files', font: headingFont, constraints: BorderLayout.NORTH)
-                             table(constraints: BorderLayout.CENTER)
-                         }
-                         vglue()
-                     }
-                     vbox(constraints: BorderLayout.EAST) {
-                         panel(constraints: BorderLayout.NORTH, border: emptyBorder(10)) {
-                             borderLayout()
-                             label('Search', font: headingFont, constraints: BorderLayout.NORTH)
-                             panel(constraints: BorderLayout.CENTER) {
-                                 borderLayout()
-                                 textField('Enter query', font: headingFont,  foreground: Color.LIGHT_GRAY, border: null, constraints: BorderLayout.NORTH)
-                                 vbox(constraints: BorderLayout.CENTER) {
-                                     hyperlink('192.168.0.1')
-                                     hyperlink('hdparm=1')
-                                     vglue()
-                                 }
-                             }
-                         }
-                         panel(constraints: BorderLayout.SOUTH, border: emptyBorder(10)) {
-                             borderLayout()
-                             label('Tags', font: headingFont, constraints: BorderLayout.NORTH)
-                             panel(constraints: BorderLayout.CENTER) {
-                                 hyperlink('network')
-                                 hyperlink('font', font: headingFont)
-                                 hyperlink('printer settings')
-                             }
-                         }
-                         vglue()
-                     }
-                 }
-                 panel(name: 'hosts') {
-                     borderLayout()
- 					comboBox(model: new DefaultComboBoxModel(new java.io.File('C:\\WINDOWS\\system32\\drivers\\etc').listFiles()), foreground: Color.LIGHT_GRAY, editable: true, border: emptyBorder(2), constraints: BorderLayout.NORTH)
- 					scrollPane(border: null) {
- 						editorPane(page: new java.net.URL('file:C:\\WINDOWS\\system32\\drivers\\etc\\hosts'), font: textFont)
- 					}
-                 }
-             }
+                     editorPane(page: new java.net.URL('file:/etc/hosts'), font: textFont)
+                 }})
+         }
 
-         swing.frame(title: 'Figurate', defaultCloseOperation: JFrame.DISPOSE_ON_CLOSE,
+         swing.edt {
+             frame(title: 'Figurate', defaultCloseOperation: JFrame.DISPOSE_ON_CLOSE,
              size: [800, 600], show: true, locationRelativeTo: null) {
              
-             lookAndFeel("system")
+//             lookAndFeel("system")
              actions() {
                  action(id: 'newFileAction', name: 'New', accelerator: shortcut('N'), closure: newTab)
                  action(id: 'openFileAction', name: 'Open', accelerator: shortcut('O'))
@@ -154,7 +123,54 @@ public class Figurate {
                      menuItem(text: "About")
                  }
              }
- 			tabbedPane(tabs)
+         tabbedPane(tabLayoutPolicy: JTabbedPane.SCROLL_TAB_LAYOUT, id: 'tabs') {
+                 panel(name: 'Home') {
+                     borderLayout()
+                     vbox(constraints: BorderLayout.CENTER) {
+                         panel(constraints: BorderLayout.CENTER, border: emptyBorder(10)) {
+                             borderLayout()
+                             label('Recent Files', font: headingFont, constraints: BorderLayout.NORTH)
+                             table(constraints: BorderLayout.CENTER)
+                         }
+                         vglue()
+                     }
+                     vbox(constraints: BorderLayout.EAST) {
+                         panel(constraints: BorderLayout.NORTH, border: emptyBorder(10)) {
+                             borderLayout()
+                             label('Search', font: headingFont, constraints: BorderLayout.NORTH)
+                             panel(constraints: BorderLayout.CENTER) {
+                                 borderLayout()
+                                 textField('Enter query', font: headingFont,  foreground: Color.LIGHT_GRAY, border: null, constraints: BorderLayout.NORTH)
+                                 vbox(constraints: BorderLayout.CENTER) {
+                                     hyperlink('192.168.0.1')
+                                     hyperlink('hdparm=1')
+                                     vglue()
+                                 }
+                             }
+                         }
+                         panel(constraints: BorderLayout.SOUTH, border: emptyBorder(10)) {
+                             borderLayout()
+                             label('Tags', font: headingFont, constraints: BorderLayout.NORTH)
+                             panel(constraints: BorderLayout.CENTER) {
+                                 hyperlink('network')
+                                 hyperlink('font', font: headingFont)
+                                 hyperlink('printer settings')
+                             }
+                         }
+                         vglue()
+                     }
+                 }
+                 panel(name: 'hosts', id: 'hostsPane') {
+                     borderLayout()
+                     comboBox(model: new DefaultComboBoxModel(new java.io.File('C:\\WINDOWS\\system32\\drivers\\etc').listFiles()), foreground: Color.LIGHT_GRAY, editable: true, border: emptyBorder(2), constraints: BorderLayout.NORTH)
+                     scrollPane(border: null) {
+                         editorPane(page: new java.net.URL('file:/etc/hosts'), font: textFont)
+                     }
+                 }
+                hostsPane.putClientProperty(SubstanceLookAndFeel.TABBED_PANE_CLOSE_BUTTONS_PROPERTY, true)
+             }
+           }
+                tabs.putClientProperty(SubstanceLookAndFeel.TABBED_PANE_CONTENT_BORDER_KIND , SubstanceConstants.TabContentPaneBorderKind.SINGLE_FULL)
          }
- 	}
+     }
 }

23ed0f783478889b5fa3a7d6e76f1d4a1e6cbf12
fortuna <fortuna@localhost>
2010-01-17 08:03:28 +0000

    Fixed about dialog

--- src/main/groovy/org/mnode/figurate/Figurate.groovy
+++ src/main/groovy/org/mnode/figurate/Figurate.groovy
@@ -406,9 +406,9 @@ class Figurate {
                      action(id: 'onlineHelpAction', name: 'Online Help', accelerator: 'F1', closure: { Desktop.desktop.browse(URI.create('http://wiki.mnode.org/figurate')) })
                      action(id: 'showTipsAction', name: 'Tips', closure: { tips.showDialog(figurateFrame) })
                      action(id: 'aboutAction', name: 'About', closure: {
-                         dialog(title: 'About Coucou', size: [300, 200], show: true, owner: figurateFrame, modal: true, locationRelativeTo: figurateFrame) {
+                         dialog(title: 'About Figurate', size: [300, 200], show: true, owner: figurateFrame, modal: true, locationRelativeTo: figurateFrame) {
                              borderLayout()
-                             label(text: 'Coucou 1.0', constraints: BorderLayout.NORTH, border: emptyBorder(10))
+                             label(text: 'Figurate 1.0', constraints: BorderLayout.NORTH, border: emptyBorder(10))
                              panel(constraints: BorderLayout.CENTER, border: emptyBorder(10)) {
                                  borderLayout()
                                  scrollPane(horizontalScrollBarPolicy: JScrollPane.HORIZONTAL_SCROLLBAR_NEVER, border: null) {
@@ -595,7 +595,7 @@ class Figurate {
                                  for (file in files) {
                                      fileModel.addElement(file)
                                  }
-                                 fileList.setModel(fileModel)
+                                 fileList.model = fileModel
                              }
                          }
                          panel(name: 'Bookmarks') {
